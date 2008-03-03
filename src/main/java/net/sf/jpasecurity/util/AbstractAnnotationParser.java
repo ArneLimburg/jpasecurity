faaf95dd643b3b23b265fc5d5e7b6d875e3d018e
Arne Limburg <arnelim@users.sourceforge.net>
2008-03-03 20:02:30 +0000

    Refactored AnnotationParsers

---
+++ src/main/java/net/sf/jpasecurity/util/AbstractAnnotationParser.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2008 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.sf.jpasecurity.util;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * This parser parses a specified <tt>Set</tt> of classes for annotations of the specified type(s).
+ * @author Arne Limburg
+ */
+public abstract class AbstractAnnotationParser<A extends Annotation> {
+
+	private Collection<Class<A>> annotationTypes;
+	
+	protected AbstractAnnotationParser() {
+	    annotationTypes = Collections.singleton(extractAnnotationType());
+    }
+    
+    protected AbstractAnnotationParser(Class<A>... annotationTypes) {
+        this.annotationTypes = Arrays.asList(annotationTypes);
+    }
+	
+    /**
+     * Parses the specified classes for the annotation(s)
+     * @param classes
+     */
+    protected void parse(Class<?>... classes) {
+        for (Class<?> annotatedClass: classes) {
+            parse(annotatedClass);
+        }
+    }  
+    
+	/**
+	 * Parses the specified classes for the annotation(s)
+	 * @param classes
+	 */
+	protected void parse(Collection<Class<?>> classes) {
+		for (Class<?> annotatedClass: classes) {
+		    parse(annotatedClass);
+		}
+	}  
+	
+	protected void parse(Class<?> annotatedClass) {
+		if (annotatedClass == null) {
+			return;
+		}
+        parse(annotatedClass.getSuperclass());
+        for (Class<?> annotatedInterface: annotatedClass.getInterfaces()) {
+            parse(annotatedInterface);
+        }
+        for (Class<A> annotationType: annotationTypes) {
+            A annotation = annotatedClass.getAnnotation(annotationType);
+            if (annotation != null) {
+                process(annotation);
+            }
+        }
+	}
+    
+    protected abstract void process(A annotation);
+
+    private Class<A> extractAnnotationType() {
+        Type type = getClass().getGenericSuperclass();
+        while (!(type instanceof ParameterizedType)
+            || !AbstractAnnotationParser.class.equals(((ParameterizedType)type).getRawType())) {
+            type = getSupertype(type);
+        }
+        Type annotationType = ((ParameterizedType)type).getActualTypeArguments()[0];
+        if (annotationType instanceof Class) {
+            return (Class<A>)annotationType;
+        } else {
+            throw new IllegalStateException("Could not determine annotation type. Please specifiy by constructor.");
+        }
+    }
+    
+    private Type getSupertype(Type type) {
+        if (type instanceof Class) {
+            return ((Class<?>)type).getGenericSuperclass();
+        } else if (type instanceof ParameterizedType) {
+            return getSupertype(((ParameterizedType)type).getRawType());
+        } else {
+            throw new IllegalStateException("Could not determine annotation type. Please specifiy by constructor.");
+        }
+    }
+}
