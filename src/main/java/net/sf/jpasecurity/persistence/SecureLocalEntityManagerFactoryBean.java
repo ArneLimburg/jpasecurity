4b1c66e4f1f5a71f83050dbf65bc0454d93781e3
Arne Limburg <arnelim@users.sourceforge.net>
2008-03-29 07:57:33 +0000

    Added spring integration

---
+++ src/main/java/net/sf/jpasecurity/persistence/SecureLocalEntityManagerFactoryBean.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2008 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.sf.jpasecurity.persistence;
+
+import java.lang.reflect.Proxy;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceException;
+import javax.persistence.spi.PersistenceUnitInfo;
+
+import net.sf.jpasecurity.security.authentication.AcegiAuthenticationProvider;
+import net.sf.jpasecurity.security.authentication.AuthenticationProvider;
+import net.sf.jpasecurity.security.rules.AccessRulesProvider;
+import net.sf.jpasecurity.security.rules.XmlAccessRulesProvider;
+
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+
+/**
+ * @author Arne Limburg
+ */
+public class SecureLocalEntityManagerFactoryBean extends LocalEntityManagerFactoryBean {
+
+    private AuthenticationProvider authenticationProvider;
+    private AccessRulesProvider accessRulesProvider;
+    
+    public AuthenticationProvider getAuthenticationProvider() {
+        if (authenticationProvider == null) {
+            authenticationProvider = new AcegiAuthenticationProvider();
+        }
+        return authenticationProvider;
+    }
+
+    public void setAuthenticationProvider(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    public AccessRulesProvider getAccessRulesProvider() {
+        if (accessRulesProvider == null) {
+            accessRulesProvider = new XmlAccessRulesProvider();
+        }
+        return accessRulesProvider;
+    }
+
+    public void setAccessRulesProvider(AccessRulesProvider accessRulesProvider) {
+        this.accessRulesProvider = accessRulesProvider;
+    }
+
+    protected EntityManagerFactory createNativeEntityManagerFactory() throws PersistenceException {
+        EntityManagerFactory entityManagerFactory = super.createNativeEntityManagerFactory();
+        Class<?> type = entityManagerFactory.getClass();
+        PersistenceUnitInfo info = SecurePersistenceProvider.createPersistenceUnitInfo(getPersistenceUnitName());
+        return (EntityManagerFactory)Proxy.newProxyInstance(type.getClassLoader(),
+                                                            EntityManagerFactoryInvocationHandler.getImplementingInterfaces(type),
+                                                            new EntityManagerFactoryInvocationHandler(entityManagerFactory,
+                                                                                                      info,
+                                                                                                      getJpaPropertyMap(),
+                                                                                                      getAuthenticationProvider(),
+                                                                                                      getAccessRulesProvider()));
+    }
+}
