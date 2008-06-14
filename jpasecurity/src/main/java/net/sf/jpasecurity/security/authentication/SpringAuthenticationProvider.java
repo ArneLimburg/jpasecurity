cb1db844e87280c7c39d20cabb192dde4ee51652
Arne Limburg <arnelim@users.sourceforge.net>
2008-06-14 09:24:50 +0000

    Added support for spring-security

---
+++ jpasecurity/src/main/java/net/sf/jpasecurity/security/authentication/SpringAuthenticationProvider.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2008 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.sf.jpasecurity.security.authentication;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.security.Authentication;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.context.SecurityContextHolder;
+
+/**
+ * @author Arne Limburg
+ */
+public class SpringAuthenticationProvider implements AuthenticationProvider {
+
+	public Object getUser() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null) {
+		    return null;
+        }
+		return authentication.getPrincipal();
+	}
+
+	public Collection<Object> getRoles() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || authentication.getAuthorities() == null) {
+		    return Collections.EMPTY_LIST;
+        }
+		List<Object> roles = new ArrayList<Object>();
+		for (GrantedAuthority authority: authentication.getAuthorities()) {
+			roles.add(authority.getAuthority());
+		}
+		return roles;
+	}
+}
