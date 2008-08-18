9edc4ef9a7992af2abc2e0c2d24245eed8e96ea6
Arne Limburg <arnelim@users.sourceforge.net>
2008-08-18 18:37:24 +0000

    Refactored tests

--- jpasecurity/src/test/java/net/sf/jpasecurity/contacts/model/User.java
+++ jpasecurity/src/test/java/net/sf/jpasecurity/contacts/model/User.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions
  * and limitations under the License.
  */
-package net.sf.jpasecurity.contacts;
+package net.sf.jpasecurity.contacts.model;
 
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
