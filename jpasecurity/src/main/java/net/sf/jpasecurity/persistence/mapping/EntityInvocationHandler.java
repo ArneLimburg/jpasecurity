81dbf3ae5f2f869dce1eebe3e8a864ea9a7aa72c
Arne Limburg <arnelim@users.sourceforge.net>
2008-09-09 20:21:38 +0000

    Added ability to update proxied objects

--- jpasecurity/src/main/java/net/sf/jpasecurity/persistence/mapping/EntityInvocationHandler.java
+++ jpasecurity/src/main/java/net/sf/jpasecurity/persistence/mapping/EntityInvocationHandler.java
@@ -17,7 +17,9 @@ package net.sf.jpasecurity.persistence.mapping;
 
 import java.lang.reflect.Method;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
 
@@ -38,7 +40,8 @@ public class EntityInvocationHandler implements MethodInterceptor {
     private SecureEntityHandler entityHandler;
     private boolean initialized;
     private Object entity;
-
+    private Map<String, Object> propertyValues = new HashMap<String, Object>();
+    
     public EntityInvocationHandler(ClassMappingInformation mapping,
                                    SecureEntityHandler entityHandler,
                                    Object entity) {
@@ -51,33 +54,49 @@ public class EntityInvocationHandler implements MethodInterceptor {
         if (!initialized) {
             initialize(object);
         }
-        return methodProxy.invokeSuper(object, args);
+        Object result = methodProxy.invokeSuper(object, args);
+        updatedChangedProperties(object);
+        return result;
     }
 
     private void initialize(Object object) {
         for (PropertyMappingInformation propertyMapping: mapping.getPropertyMappings()) {
-            if (propertyMapping instanceof RelationshipMappingInformation) {
-                RelationshipMappingInformation relationshipMapping = (RelationshipMappingInformation)propertyMapping;
-                if (relationshipMapping instanceof SingleValuedRelationshipMappingInformation) {
-                    Object relatedEntity = propertyMapping.getPropertyValue(entity);
-                    relatedEntity = entityHandler.getSecureEntity(relatedEntity);
-                    propertyMapping.setPropertyValue(object, relatedEntity);
-                } else {
-                    Collection<?> relatedEntities = (Collection<?>)propertyMapping.getPropertyValue(entity);
-                    if (relatedEntities instanceof List) {
-                        propertyMapping.setPropertyValue(object, new SecureList((List)relatedEntities, entityHandler));
-                    } else if (relatedEntities instanceof SortedSet) {
-                        propertyMapping.setPropertyValue(object, new SecureSortedSet((SortedSet)relatedEntities, entityHandler));
-                    } else if (relatedEntities instanceof Set) {
-                        propertyMapping.setPropertyValue(object, new SecureSet((Set)relatedEntities, entityHandler));
-                    } else {
-                        propertyMapping.setPropertyValue(object, new SecureCollection(relatedEntities, entityHandler));
-                    }
-                }
-            } else {
-                propertyMapping.setPropertyValue(object, propertyMapping.getPropertyValue(entity));
-            }
+            setPropertyValue(propertyMapping, entity, object);
         }
         initialized = true;
+    }
+    
+    private void updatedChangedProperties(Object object) {
+        for (PropertyMappingInformation propertyMapping: mapping.getPropertyMappings()) {
+            if (propertyMapping.getPropertyValue(object) != propertyValues.get(propertyMapping.getPropertyName())) {
+                setPropertyValue(propertyMapping, object, entity);
+            }
+        }
+    }
+    
+    private void setPropertyValue(PropertyMappingInformation propertyMapping, Object source, Object target) {
+        Object value;
+        if (propertyMapping instanceof RelationshipMappingInformation) {
+            RelationshipMappingInformation relationshipMapping = (RelationshipMappingInformation)propertyMapping;
+            if (relationshipMapping instanceof SingleValuedRelationshipMappingInformation) {
+                Object relatedEntity = propertyMapping.getPropertyValue(source);
+                value = entityHandler.getSecureEntity(relatedEntity);
+            } else {
+                Collection<?> relatedEntities = (Collection<?>)propertyMapping.getPropertyValue(source);
+                if (relatedEntities instanceof List) {
+                    value = new SecureList((List)relatedEntities, entityHandler);
+                } else if (relatedEntities instanceof SortedSet) {
+                    value = new SecureSortedSet((SortedSet)relatedEntities, entityHandler);
+                } else if (relatedEntities instanceof Set) {
+                    value = new SecureSet((Set)relatedEntities, entityHandler);
+                } else {
+                    value = new SecureCollection(relatedEntities, entityHandler);
+                }
+            }
+        } else {
+            value = propertyMapping.getPropertyValue(source);
+        }
+        propertyMapping.setPropertyValue(target, value);
+        propertyValues.put(propertyMapping.getPropertyName(), value);        
     }
 }
