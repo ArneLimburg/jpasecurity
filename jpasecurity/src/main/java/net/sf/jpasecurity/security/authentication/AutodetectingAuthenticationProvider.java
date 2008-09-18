e1d56bf407d0e209ff6a1aaa1aaa2274f586de8f
Arne Limburg <arnelim@users.sourceforge.net>
2008-09-18 18:56:31 +0000

    Added DefaultAccessRulesProvider and AutodetectingAuthenticationProvider

---
+++ jpasecurity/src/main/java/net/sf/jpasecurity/security/authentication/AutodetectingAuthenticationProvider.java
@@ -0,0 +1,56 @@
+package net.sf.jpasecurity.security.authentication;
+
+import java.util.Collection;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class AutodetectingAuthenticationProvider implements AuthenticationProvider {
+
+    private static final String SPRING_CONTEXT_HOLDER_CLASS
+        = "org.springframework.security.context.SecurityContextHolder";
+    private static final String ACEGI_CONTEXT_HOLDER_CLASS
+        = "org.acegisecurity.context.SecurityContextHolder";
+    private static final Log LOG = LogFactory.getLog(AutodetectingAuthenticationProvider.class);
+    
+    private AuthenticationProvider authenticationProvider;
+    
+    public AutodetectingAuthenticationProvider() {
+        authenticationProvider = autodetectAuthenticationProvider();
+    }
+    
+    protected AuthenticationProvider autodetectAuthenticationProvider() {
+        try {
+            Class.forName(SPRING_CONTEXT_HOLDER_CLASS);
+            LOG.info("autodetected presence of Spring Security, using SpringAuthenticationProvider");
+            return new SpringAuthenticationProvider();
+        } catch (ClassNotFoundException springSecurityNotFoundException) {
+            try {
+                Class.forName(ACEGI_CONTEXT_HOLDER_CLASS);
+                LOG.info("autodetected presence of Acegi Security, using AcegiAuthenticationProvider");
+                return new AcegiAuthenticationProvider();
+            } catch (ClassNotFoundException acegiSecurityNotFoundException) {
+                try {
+                    InitialContext context = new InitialContext();
+                    context.lookup("java:comp/EJBContext");
+                    LOG.info("autodetected presence of EJB, using EJBAuthenticationProvider");
+                    return new EjbAuthenticationProvider();
+                } catch (NamingException ejbSecurityNotFoundException) {
+                    LOG.info("falling back to DefaultAuthenticationPovider");
+                    return new DefaultAuthenticationProvider();
+                }
+            }
+        }
+    }
+    
+    public Object getUser() {
+        return authenticationProvider.getUser();
+    }
+
+    public Collection<Object> getRoles() {
+        return authenticationProvider.getRoles();
+    }
+}
---
+++ jpasecurity/src/main/java/net/sf/jpasecurity/security/rules/DefaultAccessRulesProvider.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2008 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.sf.jpasecurity.security.rules;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.jpasecurity.persistence.PersistenceInformationReceiver;
+import net.sf.jpasecurity.persistence.mapping.MappingInformation;
+
+/**
+ * @author Arne Limburg
+ */
+public class DefaultAccessRulesProvider implements AccessRulesProvider, PersistenceInformationReceiver {
+
+    private final AnnotationAccessRulesProvider annotationRulesProvider = new AnnotationAccessRulesProvider();
+    private final XmlAccessRulesProvider xmlRulesProvider = new XmlAccessRulesProvider();
+    private List<AccessRule> accessRules;
+    
+    public List<AccessRule> getAccessRules() {
+        if (accessRules == null) {
+            List<AccessRule> accessRules = new ArrayList<AccessRule>();
+            accessRules.addAll(annotationRulesProvider.getAccessRules());
+            accessRules.addAll(xmlRulesProvider.getAccessRules());
+            this.accessRules = Collections.unmodifiableList(accessRules);
+        }
+        return accessRules;
+    }
+
+    public void setPersistenceMapping(MappingInformation persistenceMapping) {
+        annotationRulesProvider.setPersistenceMapping(persistenceMapping);
+        xmlRulesProvider.setPersistenceMapping(persistenceMapping);
+    }
+
+    public void setPersistenceProperties(Map<String, String> properties) {
+        annotationRulesProvider.setPersistenceProperties(properties);
+        xmlRulesProvider.setPersistenceProperties(properties);
+    }
+}
