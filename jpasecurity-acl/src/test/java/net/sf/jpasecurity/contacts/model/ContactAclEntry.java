720a0d2f4e3a594e3cd2ece30081674ac05b0d30
Arne Limburg <arnelim@users.sourceforge.net>
2008-09-20 15:06:15 +0000

    Removed Id attribute

--- jpasecurity-acl/src/test/java/net/sf/jpasecurity/contacts/model/ContactAclEntry.java
+++ jpasecurity-acl/src/test/java/net/sf/jpasecurity/contacts/model/ContactAclEntry.java
@@ -4,18 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.ManyToOne;
 
 import net.sf.jpasecurity.acl.AclEntry;
-import net.sf.jpasecurity.acl.AclEntryIdentifier;
 
 @Entity
-//@IdClass(AclEntryIdentifier.class)
 public class ContactAclEntry extends AclEntry<Contact, User> {
 
-	private int id;
-	
 	protected ContactAclEntry() {
 		//for subclassing only
 	}
@@ -28,11 +23,7 @@ public class ContactAclEntry extends AclEntry<Contact, User> {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
+		return super.getId();
 	}
 	
 	@ManyToOne
