cb775ed777022fc7f68ed622cf4739ed1a326727
Arne Limburg <arnelim@users.sourceforge.net>
2008-10-03 13:20:58 +0000

    Fixed bug where not every @PermitWhere annotation was processed during parsing

--- jpasecurity/src/main/java/net/sf/jpasecurity/security/rules/PermissionParser.java
+++ jpasecurity/src/main/java/net/sf/jpasecurity/security/rules/PermissionParser.java
@@ -16,27 +16,41 @@
 
 package net.sf.jpasecurity.security.rules;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import net.sf.jpasecurity.util.AbstractAnnotationParser;
 
 /**
- * This class parses classes for the {@link PermitWhere} annotation.
+ * This class parses classes for the {@link PermitWhere} and {@link PermitAny} annotations.
  * @author Arne Limburg
  */
 public class PermissionParser extends AbstractAnnotationParser<PermitAny> {
 
-    private PermitWhereParser permitWhereParser;
-    
-    public Map<Class<?>, PermitWhere> parsePermissions(Class<?>... classes) {
-        permitWhereParser = new PermitWhereParser();
+    private Map<Class<?>, List<PermitWhere>> permissions;
+
+    public Map<Class<?>, List<PermitWhere>> parsePermissions(Class<?>... classes) {
+        PermitWhereParser permitWhereParser = new PermitWhereParser();
+        permissions = new HashMap<Class<?>, List<PermitWhere>>();
         parse(classes);
-        return permitWhereParser.parsePermissions(classes);
+        for (Map.Entry<Class<?>, PermitWhere> annotation: permitWhereParser.parsePermissions(classes).entrySet()) {
+            List<PermitWhere> annotations = permissions.get(annotation.getKey());
+            if (annotations == null) {
+                annotations = new ArrayList<PermitWhere>();
+                permissions.put(annotation.getKey(), annotations);
+            }
+            annotations.add(annotation.getValue());
+        }
+        return permissions;
     }
 
     protected void process(Class<?> annotatedClass, PermitAny permitAny) {
+        List<PermitWhere> annotations = new ArrayList<PermitWhere>();
+        permissions.put(annotatedClass, annotations);
         for (PermitWhere permitWhere: permitAny.value()) {
-            permitWhereParser.process(annotatedClass, permitWhere);
+            annotations.add(permitWhere);
         }
     }
 }
