f5df2a367cef4ab24f500e0388a6d3e4d7f1f4f5
Arne Limburg <arnelim@users.sourceforge.net>
2008-10-27 19:29:22 +0000

    Moved ToStringVisitorTest into subpackage parser

--- jpasecurity/src/test/java/net/sf/jpasecurity/jpql/parser/ToStringVisitorTest.java
+++ jpasecurity/src/test/java/net/sf/jpasecurity/jpql/parser/ToStringVisitorTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions
  * and limitations under the License.
  */
-package net.sf.jpasecurity.jpql;
+package net.sf.jpasecurity.jpql.parser;
 
 import junit.framework.TestCase;
 import net.sf.jpasecurity.jpql.parser.JpqlAccessRule;
