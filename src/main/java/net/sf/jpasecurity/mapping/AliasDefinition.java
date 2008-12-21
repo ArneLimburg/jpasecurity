72a96f012449ec2b1616f0a4100e60d8b42b1862
Arne Limburg <arnelim@users.sourceforge.net>
2008-12-21 01:33:15 +0000

    Added in-memory evaluation for EXISTS

---
+++ src/main/java/net/sf/jpasecurity/mapping/AliasDefinition.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2008 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.sf.jpasecurity.mapping;
+
+/**
+ * @author Arne Limburg
+ */
+public class AliasDefinition {
+
+    private String alias;
+    private Class<?> type;
+    private String joinPath;
+    private boolean innerJoin;
+
+    public AliasDefinition(String alias, Class<?> type) {
+        this.alias = alias;
+        this.type = type;
+    }
+
+    public AliasDefinition(String alias, Class<?> type, String joinPath, boolean innerJoin) {
+        this(alias, type);
+        this.joinPath = joinPath;
+        this.innerJoin = innerJoin;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getJoinPath() {
+        return joinPath;
+    }
+
+    public boolean isJoin() {
+        return getJoinPath() != null;
+    }
+
+    public boolean isInnerJoin() {
+        return isJoin() && innerJoin;
+    }
+
+    public boolean isOuterJoin() {
+        return isJoin() && !innerJoin;
+    }
+
+    public Class<?> getType() {
+        return type;
+    }
+}
