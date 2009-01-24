a87ae8d7977e4e8511544e2fd15abc17a0641a43
Arne Limburg <arnelim@users.sourceforge.net>
2009-01-24 22:08:04 +0000

    Renamed method hasSetter to hasPropertySetter

--- jpasecurity/src/main/java/net/sf/jpasecurity/mapping/parser/AbstractMappingParser.java
+++ jpasecurity/src/main/java/net/sf/jpasecurity/mapping/parser/AbstractMappingParser.java
@@ -341,11 +341,11 @@ public abstract class AbstractMappingParser {
     protected FetchType getFetchType(Member property) {
         Class<?> type = getType(property);
         if (Collection.class.isAssignableFrom(type) || Map.class.isAssignableFrom(type)) {
-        	return FetchType.LAZY;
+            return FetchType.LAZY;
         }
         return FetchType.EAGER;
     }
-    
+
     protected abstract CascadeType[] getCascadeTypes(Member property);
 
     protected boolean isRelationshipProperty(Member property) {
@@ -369,10 +369,10 @@ public abstract class AbstractMappingParser {
             = SET_PROPERTY_PREFIX + Character.toUpperCase(propertyName.charAt(0)) + propertyName.substring(1);
         Class<?> entityClass = method.getDeclaringClass();
         Class<?> propertyType = method.getReturnType();
-        return hasSetter(entityClass, propertySetterName, propertyType);
+        return hasPropertySetter(entityClass, propertySetterName, propertyType);
     }
 
-    private boolean hasSetter(Class<?> entityClass, String propertySetterName, Class<?> propertyType) {
+    private boolean hasPropertySetter(Class<?> entityClass, String propertySetterName, Class<?> propertyType) {
         if (entityClass == null) {
             return false;
         }
@@ -383,7 +383,7 @@ public abstract class AbstractMappingParser {
                 return method.getParameterTypes()[0].isAssignableFrom(propertyType);
             }
         }
-        return hasSetter(entityClass.getSuperclass(), propertySetterName, propertyType);
+        return hasPropertySetter(entityClass.getSuperclass(), propertySetterName, propertyType);
     }
 
     private ClassLoader findClassLoader(PersistenceUnitInfo persistenceUnit) {
