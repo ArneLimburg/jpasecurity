35917694f6f4fff6867e9d7b81d52f5ca94bb595
Arne Limburg <arnelim@users.sourceforge.net>
2009-02-08 11:22:47 +0000

    Added RoleAllowed and RolesAllowed annotations

--- jpasecurity/src/main/java/net/sf/jpasecurity/security/rules/RoleAllowedParser.java
+++ jpasecurity/src/main/java/net/sf/jpasecurity/security/rules/RoleAllowedParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008, 2009 Arne Limburg
+ * Copyright 2009 Arne Limburg
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,35 +20,23 @@ import java.util.Map;
 
 import net.sf.jpasecurity.AccessType;
 import net.sf.jpasecurity.security.RoleAllowed;
-import net.sf.jpasecurity.security.RolesAllowed;
 import net.sf.jpasecurity.util.AbstractAnnotationParser;
 
 /**
  * <strong>This class is not thread-safe.</strong>
  * @author Arne Limburg
  */
-public class RolesAllowedParser extends AbstractAnnotationParser<RolesAllowed> {
+public class RoleAllowedParser extends AbstractAnnotationParser<RoleAllowed> {
 
     private Map<String, AccessType[]> rolesAllowed = new HashMap<String, AccessType[]>();
 
     public Map<String, AccessType[]> parseAllowedRoles(Class<?>... classes) {
         rolesAllowed.clear();
-        EjbRolesAllowedParser rolesAllowedParser = new EjbRolesAllowedParser();
-        for (String role: rolesAllowedParser.parseAllowedRoles(classes)) {
-            rolesAllowed.put(role, AccessType.ALL);
-        }
-        RoleAllowedParser roleAllowedParser = new RoleAllowedParser();
-        rolesAllowed.putAll(roleAllowedParser.parseAllowedRoles(classes));
         parse(classes);
         return rolesAllowed;
     }
 
-    protected void process(RolesAllowed annotation) {
-        for (RoleAllowed roleAllowed: annotation.value()) {
-            rolesAllowed.put(roleAllowed.role(), roleAllowed.access());
-        }
-        for (String role: annotation.roles()) {
-            rolesAllowed.put(role, annotation.access());
-        }
+    protected void process(RoleAllowed roleAllowed) {
+        rolesAllowed.put(roleAllowed.role(), roleAllowed.access());
     }
 }
