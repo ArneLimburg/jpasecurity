0233d9b523caf9da437298025bb759aa53d23d08
Arne Limburg <arnelim@users.sourceforge.net>
2010-03-16 15:17:17 +0000

    Fixed bug in MappingEvaluator where types of join paths where not resolved correctly and improved InMemoryEvaluator

--- jpasecurity/src/test/java/net/sf/jpasecurity/model/acl/AbstractTrademarkRelatedEntity.java
+++ jpasecurity/src/test/java/net/sf/jpasecurity/model/acl/AbstractTrademarkRelatedEntity.java
@@ -1,13 +1,17 @@
 package net.sf.jpasecurity.model.acl;
 
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 
 @EntityListeners({
    TrademarkListener.class
 })
+@MappedSuperclass
 public class AbstractTrademarkRelatedEntity extends AbstractEntity {
 
+   @Basic
    @Column(name = "TRADEMARK_ID", nullable = false)
    private Long trademarkId;
 
