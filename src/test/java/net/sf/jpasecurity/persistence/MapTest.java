fd9ea0c5c952257aa3690155f474fc455676786f
Arne Limburg <arnelim@users.sourceforge.net>
2010-03-29 11:02:21 +0000

    Added copyright notice

--- src/test/java/net/sf/jpasecurity/persistence/MapTest.java
+++ src/test/java/net/sf/jpasecurity/persistence/MapTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package net.sf.jpasecurity.persistence;
 
 import javax.persistence.EntityManager;
@@ -10,6 +25,9 @@ import net.sf.jpasecurity.model.FieldAccessMapKey;
 import net.sf.jpasecurity.model.FieldAccessMapValue;
 import net.sf.jpasecurity.security.authentication.TestAuthenticationProvider;
 
+/**
+ * @author Arne Limburg
+ */
 public class MapTest extends TestCase {
 
     public static final String USER1 = "user1";
