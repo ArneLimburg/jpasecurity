704ed1ff2b2ed0e91502159f55507482de204ede
Arne Limburg <arnelim@users.sourceforge.net>
2010-04-08 19:52:15 +0000

    Fixed bug where Subselect was not evaluated correctly when WHERE-clause could be evaluated to false

--- jpasecurity/src/test/java/net/sf/jpasecurity/acl/AclSyntaxTest.java
+++ jpasecurity/src/test/java/net/sf/jpasecurity/acl/AclSyntaxTest.java
@@ -2,6 +2,7 @@ package net.sf.jpasecurity.acl;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 
 import junit.framework.TestCase;
@@ -51,9 +52,26 @@ public class AclSyntaxTest extends TestCase {
        entityManager.getTransaction().commit();
        entityManager.close();
    }
+    
+   public void tearDown() {
+       TestAuthenticationProvider.authenticate(null);
+   }
    
    public void testAclProtectedEntityAccess() {
        EntityManager entityManager = entityManagerFactory.createEntityManager();
        AclProtectedEntity entity = (AclProtectedEntity)entityManager.createQuery("select e from AclProtectedEntity e").getSingleResult();
+       entityManager.close();
+   }
+
+   public void testAclProtectedEntityAccessWithNoPrivileges() {
+       TestAuthenticationProvider.authenticate(TRADEMARK_ID);
+       EntityManager entityManager = entityManagerFactory.createEntityManager();
+       try {
+           entityManager.createQuery("select e from AclProtectedEntity e").getSingleResult();
+           fail();
+       } catch (NoResultException e) {
+           //expected
+       }
+       entityManager.close();
    }
 }
