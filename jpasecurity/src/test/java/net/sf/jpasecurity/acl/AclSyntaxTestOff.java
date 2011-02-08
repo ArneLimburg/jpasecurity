2a97931fa2a4760832e40141e441d1cc9ac719ce
Stefan Hildebrandt <jpasecurity@hildebrandt.tk>
2011-02-08 13:56:31 +0000

    Testcase demonstrating a problem with complex acl-rules in in memory evaluator

--- jpasecurity/src/test/java/net/sf/jpasecurity/acl/AclSyntaxTestOff.java
+++ jpasecurity/src/test/java/net/sf/jpasecurity/acl/AclSyntaxTestOff.java
@@ -1,9 +1,13 @@
 package net.sf.jpasecurity.acl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import junit.framework.TestCase;
 import net.sf.jpasecurity.model.acl.Acl;
@@ -11,9 +15,11 @@ import net.sf.jpasecurity.model.acl.AclEntry;
 import net.sf.jpasecurity.model.acl.AclProtectedEntity;
 import net.sf.jpasecurity.model.acl.Group;
 import net.sf.jpasecurity.model.acl.Privilege;
+import net.sf.jpasecurity.model.acl.Role;
+import net.sf.jpasecurity.model.acl.User;
 import net.sf.jpasecurity.security.authentication.TestAuthenticationProvider;
 
-public class AclSyntaxTest extends TestCase {
+public class AclSyntaxTestOff extends TestCase {
 
     public static final Long TRADEMARK_ID = 1L;
     
@@ -21,7 +27,8 @@ public class AclSyntaxTest extends TestCase {
     private Group group;
     private Privilege privilege1;
     private Privilege privilege2;
-    
+    private User user;
+
     public void setUp() {
        entityManagerFactory = Persistence.createEntityManagerFactory("acl-model");
        EntityManager entityManager = entityManagerFactory.createEntityManager();
@@ -35,7 +42,17 @@ public class AclSyntaxTest extends TestCase {
        group = new Group();
        group.setName("USERS");
        entityManager.persist(group);
-       TestAuthenticationProvider.authenticate(TRADEMARK_ID, group, privilege1, privilege2);
+       Role role = new Role();
+       role.setName("Test Role");
+       role.setPrivileges(Arrays.asList(privilege1, privilege2));
+       entityManager.persist(role);
+       user = new User();
+       user.setGroups(Arrays.asList(group));
+       user.setRoles(Arrays.asList(role));
+       entityManager.persist(user);
+       entityManager.getTransaction().commit();
+       entityManager.getTransaction().begin();
+       TestAuthenticationProvider.authenticate(TRADEMARK_ID, user.getId());
 
        Acl acl = new Acl();
        acl.setTrademarkId(TRADEMARK_ID);
