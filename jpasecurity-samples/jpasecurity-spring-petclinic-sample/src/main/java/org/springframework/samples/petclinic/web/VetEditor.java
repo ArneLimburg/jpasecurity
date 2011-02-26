92057afcacf25e8590feb8bcb2e4f4a2e7cade63
Arne Limburg <arnelim@users.sourceforge.net>
2011-02-26 13:20:00 +0000

    Checkstyle fix

--- jpasecurity-samples/jpasecurity-spring-petclinic-sample/src/main/java/org/springframework/samples/petclinic/web/VetEditor.java
+++ jpasecurity-samples/jpasecurity-spring-petclinic-sample/src/main/java/org/springframework/samples/petclinic/web/VetEditor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008 - 2011 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package org.springframework.samples.petclinic.web;
 
 import java.beans.PropertyEditorSupport;
@@ -17,7 +32,7 @@ public class VetEditor extends PropertyEditorSupport {
 	}
 
 	@Override
-	public void setAsText(String text) throws IllegalArgumentException {
+	public void setAsText(String text) {
 		for (Vet vet: this.clinic.getVets()) {
 			if (vet.toString().equals(text)) {
 				setValue(vet);
