4732a89479d552065e3568d5b1bc53ab51fdd777
Arne Limburg <arnelim@users.sourceforge.net>
2011-05-02 20:49:58 +0000

    Added license header

--- jpasecurity-core/src/test/java/net/sf/jpasecurity/model/acl/Role.java
+++ jpasecurity-core/src/test/java/net/sf/jpasecurity/model/acl/Role.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Stefan Hildebrandt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package net.sf.jpasecurity.model.acl;
 
 import java.util.ArrayList;
@@ -11,10 +26,10 @@ import javax.persistence.ManyToMany;
 
 @Entity
 public class Role extends AbstractEntity {
-   @Column(name = "NAME", length = 50)
+   @Column(name = "NAME")
    private String name;
 
-   @Column(name = "DESCRIPTION", length = 256)
+   @Column(name = "DESCRIPTION")
    private String description;
 
    @ManyToMany
