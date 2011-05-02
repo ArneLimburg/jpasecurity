a40018055e885eae750d36dcd6d1811b07b0288b
Arne Limburg <arnelim@users.sourceforge.net>
2011-05-02 20:49:58 +0000

    Added license header

--- jpasecurity-core/src/test/java/net/sf/jpasecurity/model/acl/Privilege.java
+++ jpasecurity-core/src/test/java/net/sf/jpasecurity/model/acl/Privilege.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Stefan Hildebrandt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package net.sf.jpasecurity.model.acl;
 
 import javax.persistence.Column;
@@ -6,11 +21,11 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 
 @Entity
-public class Privilege extends AbstractEntity{
-   @Column(name = "NAME", length = 50)
+public class Privilege extends AbstractEntity {
+   @Column(name = "NAME")
    private String name;
 
-   @Column(name = "DESCRIPTION", length = 256)
+   @Column(name = "DESCRIPTION")
    private String description;
 
    @Enumerated(value = EnumType.STRING)
