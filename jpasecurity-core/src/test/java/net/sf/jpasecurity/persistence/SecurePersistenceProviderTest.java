64a505ba7ff02c6867db0e5634c2b3fa4b93cfd5
Arne Limburg <arnelim@users.sourceforge.net>
2011-05-03 13:47:38 +0000

    Migrated to JUnit 4

--- jpasecurity-core/src/test/java/net/sf/jpasecurity/persistence/SecurePersistenceProviderTest.java
+++ jpasecurity-core/src/test/java/net/sf/jpasecurity/persistence/SecurePersistenceProviderTest.java
@@ -15,11 +15,17 @@
  */
 package net.sf.jpasecurity.persistence;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.spi.PersistenceUnitInfo;
@@ -29,44 +35,53 @@ import net.sf.jpasecurity.model.FieldAccessAnnotationTestBean;
 import net.sf.jpasecurity.model.FieldAccessMapKey;
 import net.sf.jpasecurity.model.FieldAccessMapValue;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 /**
  * @author Arne Limburg
  */
-public class SecurePersistenceProviderTest extends TestCase {
+public class SecurePersistenceProviderTest {
 
     private SecurePersistenceProvider securePersistenceProvider = new SecurePersistenceProvider();
-    
-    public void testWrongPersistenceProvider() {
+
+    @Test
+    public void wrongPersistenceProvider() {
         assertNull(securePersistenceProvider.createEntityManagerFactory("hibernate", null));
         assertNull(securePersistenceProvider.createEntityManagerFactory("hibernate", Collections.EMPTY_MAP));
     }
-    
-    public void testOverriddenPersistenceProvider() {
+
+    @Test
+    public void overriddenPersistenceProvider() {
         Map<String, String> persistenceProperties = new HashMap<String, String>();
-        persistenceProperties.put(SecurePersistenceProvider.PERSISTENCE_PROVIDER_PROPERTY, SecurePersistenceProvider.class.getName());
+        persistenceProperties.put(SecurePersistenceProvider.PERSISTENCE_PROVIDER_PROPERTY,
+                                  SecurePersistenceProvider.class.getName());
         assertNotNull(securePersistenceProvider.createEntityManagerFactory("hibernate", persistenceProperties));
     }
-    
-    public void testNoPersistenceXml() {
+
+    @Test
+    public void noPersistenceXml() {
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
         Thread.currentThread().setContextClassLoader(new URLClassLoader(new URL[0], null));
-        assertNull(securePersistenceProvider.createEntityManagerFactory("annotation-based-method-access", Collections.EMPTY_MAP));
+        assertNull(securePersistenceProvider.createEntityManagerFactory("annotation-based-method-access",
+                                                                        Collections.EMPTY_MAP));
         Thread.currentThread().setContextClassLoader(contextClassLoader);
     }
 
-    public void testPersistenceUnitNotFound() {
+    @Test
+    public void persistenceUnitNotFound() {
         assertNull(securePersistenceProvider.createEntityManagerFactory("bogus-unit", Collections.EMPTY_MAP));
     }
-    
-    public void testCreateContainerEntityManagerFactory() {
+
+    @Test
+    public void createContainerEntityManagerFactory() {
         EntityManagerFactory entityManagerFactory
-            = securePersistenceProvider.createContainerEntityManagerFactory(createPersistenceUnitInfo(), Collections.EMPTY_MAP);
+            = securePersistenceProvider.createContainerEntityManagerFactory(createPersistenceUnitInfo(),
+                                                                            Collections.EMPTY_MAP);
         assertTrue(entityManagerFactory.createEntityManager() instanceof SecureEntityManager);
     }
-    
-    public void testCreateLightEntityManagerFactory() {
+
+    @Test
+    public void createLightEntityManagerFactory() {
         PersistenceUnitInfo info = createPersistenceUnitInfo();
         info.getProperties().put(SecurePersistenceProvider.SECURE_PERSISTENCE_PROVIDER_TYPE_PROPERTY,
                                  SecurePersistenceProvider.SECURE_PERSISTENCE_PROVIDER_TYPE_LIGHT);
@@ -75,7 +90,8 @@ public class SecurePersistenceProviderTest extends TestCase {
         assertFalse(entityManagerFactory.createEntityManager() instanceof SecureEntityManager);
     }
 
-    public void testOverriddenLightEntityManagerFactory() {
+    @Test
+    public void overriddenLightEntityManagerFactory() {
         Map<String, String> properties
             = Collections.singletonMap(SecurePersistenceProvider.SECURE_PERSISTENCE_PROVIDER_TYPE_PROPERTY,
                                        SecurePersistenceProvider.SECURE_PERSISTENCE_PROVIDER_TYPE_LIGHT);
@@ -94,15 +110,18 @@ public class SecurePersistenceProviderTest extends TestCase {
         persistenceUnitInfo.getManagedClassNames().add(FieldAccessMapKey.class.getName());
         persistenceUnitInfo.getManagedClassNames().add(FieldAccessMapValue.class.getName());
         persistenceUnitInfo.setExcludeUnlistedClasses(true);
-        persistenceUnitInfo.getProperties().put("net.sf.jpasecurity.persistence.provider", "org.hibernate.ejb.HibernatePersistence");
-        persistenceUnitInfo.getProperties().put("net.sf.jpasecurity.security.authentication.provider", "net.sf.jpasecurity.security.authentication.TestAuthenticationProvider");
-        persistenceUnitInfo.getProperties().put("net.sf.jpasecurity.security.rules.provider", "net.sf.jpasecurity.security.rules.XmlAccessRulesProvider");
-        persistenceUnitInfo.getProperties().put("hibernate.hbm2ddl.auto", "create-drop");
-        persistenceUnitInfo.getProperties().put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-        persistenceUnitInfo.getProperties().put("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
-        persistenceUnitInfo.getProperties().put("hibernate.connection.url", "jdbc:hsqldb:mem:test");
-        persistenceUnitInfo.getProperties().put("hibernate.connection.username", "sa");
-        persistenceUnitInfo.getProperties().put("hibernate.connection.password", "");
+        Properties properties = persistenceUnitInfo.getProperties();
+        properties.put("net.sf.jpasecurity.persistence.provider", "org.hibernate.ejb.HibernatePersistence");
+        properties.put("net.sf.jpasecurity.security.authentication.provider",
+                       "net.sf.jpasecurity.security.authentication.TestAuthenticationProvider");
+        properties.put("net.sf.jpasecurity.security.rules.provider",
+                       "net.sf.jpasecurity.security.rules.XmlAccessRulesProvider");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.put("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
+        properties.put("hibernate.connection.url", "jdbc:hsqldb:mem:test");
+        properties.put("hibernate.connection.username", "sa");
+        properties.put("hibernate.connection.password", "");
         return persistenceUnitInfo;
     }
 }
