0e1be518080a0c45f875ffc763ea2882f6961c06
Arne Limburg <arnelim@users.sourceforge.net>
2011-05-04 21:35:17 +0000

    Added test

--- jpasecurity-core/src/test/java/net/sf/jpasecurity/entity/SecureSetTest.java
+++ jpasecurity-core/src/test/java/net/sf/jpasecurity/entity/SecureSetTest.java
@@ -15,11 +15,22 @@
  */
 package net.sf.jpasecurity.entity;
 
+import static net.sf.jpasecurity.AccessType.READ;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.HashSet;
 import java.util.Set;
 
+import net.sf.jpasecurity.AccessManager;
 import net.sf.jpasecurity.SecureCollection;
 import net.sf.jpasecurity.SecureEntity;
+
+import org.junit.Test;
 
 /**
  * @author Arne Limburg
@@ -35,5 +46,25 @@ public class SecureSetTest extends AbstractSecureCollectionTestCase {
             filtered.add(secureEntity);
         }
         return new SecureSet<Object>(original, filtered, objectManager);
+    }
+
+    @Test
+    public void filter() {
+
+        Object object1 = new Object();
+        Object object2 = new Object();
+        AccessManager accessManager = createMock(AccessManager.class);
+        expect(accessManager.isAccessible(READ, object1)).andReturn(true);
+        expect(accessManager.isAccessible(READ, object2)).andReturn(false);
+        replay(accessManager);
+
+        Set<Object> original = new HashSet<Object>();
+        original.add(object1);
+        original.add(object2);
+
+        SecureSet<Object> filtered = new SecureSet<Object>(original, getObjectManager(), accessManager);
+        assertEquals(1, filtered.size());
+        assertTrue(filtered.contains(object1));
+        assertFalse(filtered.contains(object2));
     }
 }
