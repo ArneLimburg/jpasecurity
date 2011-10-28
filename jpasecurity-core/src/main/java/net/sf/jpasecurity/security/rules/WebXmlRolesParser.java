af68716f611e2318bffcfee2c7ad434fb6642886
Arne Limburg <arnelim@users.sourceforge.net>
2011-10-28 09:58:18 +0000

    Added WebXmlRolesParser

---
+++ jpasecurity-core/src/main/java/net/sf/jpasecurity/security/rules/WebXmlRolesParser.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2011 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.sf.jpasecurity.security.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.jpasecurity.ExceptionFactory;
+import net.sf.jpasecurity.configuration.DefaultExceptionFactory;
+import net.sf.jpasecurity.security.rules.WebXmlRolesParser.XmlRolesHandler;
+import net.sf.jpasecurity.xml.AbstractXmlParser;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * This parser reads declared roles from the web.xml
+ * @author Arne Limburg
+ */
+public class WebXmlRolesParser extends AbstractXmlParser<XmlRolesHandler> {
+    
+    public WebXmlRolesParser() {
+        this(new DefaultExceptionFactory());
+    }
+
+    public WebXmlRolesParser(ExceptionFactory factory) {
+        super(new XmlRolesHandler(), factory);
+    }
+
+    public List<String> getRoles() {
+        return getHandler().getRoles();
+    }
+
+    protected static class XmlRolesHandler extends DefaultHandler {
+
+        private static final String ROLE_NAME_TAG = "role-name";
+
+        private StringBuilder currentText = new StringBuilder();
+        private List<String> roles = new ArrayList<String>();
+
+        public List<String> getRoles() {
+            return roles;
+        }
+
+        public void startElement(String uri, String tag, String qualified, Attributes attributes) throws SAXException {
+            if (ROLE_NAME_TAG.equals(qualified)) {
+                currentText.setLength(0);
+            }
+        }
+
+        public void characters(char[] chars, int start, int length) throws SAXException {
+            currentText.append(chars, start, length);
+        }
+
+        public void endElement(String uri, String localName, String qualifiedName) throws SAXException {
+            String text = currentText.toString().trim();
+            currentText.setLength(0);
+            if (ROLE_NAME_TAG.equals(qualifiedName)) {
+                roles.add(text);
+            }
+        }
+    }
+}
