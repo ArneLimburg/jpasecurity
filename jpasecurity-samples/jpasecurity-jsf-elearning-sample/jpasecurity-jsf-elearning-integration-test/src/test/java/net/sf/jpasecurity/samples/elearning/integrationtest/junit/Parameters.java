27fc26bd18942eda5bc25299affff7848af60f56
Arne Limburg <arnelim@users.sourceforge.net>
2011-11-23 09:27:56 +0000

    Implemented parameterization of tests

---
+++ jpasecurity-samples/jpasecurity-jsf-elearning-sample/jpasecurity-jsf-elearning-integration-test/src/test/java/net/sf/jpasecurity/samples/elearning/integrationtest/junit/Parameters.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2011 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.sf.jpasecurity.samples.elearning.integrationtest.junit;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * An annotation to specify urls against that integration tests should run.
+ * @author Arne Limburg
+ */
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface Parameters {
+    String[] value() default {};
+}
