6474bcb16fa1cbdb23c3df23d05c3568cb482cca
Arne Limburg <arnelim@users.sourceforge.net>
2011-11-24 20:49:22 +0000

    Introduced UserService

---
+++ jpasecurity-samples/jpasecurity-jsf-elearning-sample/jpasecurity-jsf-elearning-jsf-application/src/main/java/net/sf/jpasecurity/samples/elearning/jsf/service/UserService.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2011 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.sf.jpasecurity.samples.elearning.jsf.service;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import net.sf.jpasecurity.sample.elearning.domain.Course;
+import net.sf.jpasecurity.sample.elearning.domain.Name;
+import net.sf.jpasecurity.sample.elearning.domain.User;
+import net.sf.jpasecurity.sample.elearning.domain.UserRepository;
+
+/**
+ * @author Arne Limburg
+ */
+@RequestScoped @ManagedBean
+public class UserService {
+
+    @ManagedProperty(value = "#{course.entity}")
+    private Course currentCourse;
+    @ManagedProperty(value = "#{userRepository}")
+    private UserRepository userRepository;
+
+    public <U extends User> U getCurrentUser() {
+        return userRepository.<U>findUser(getCurrentUserName());
+    }
+
+    public Name getCurrentUserName() {
+        String user = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
+        return user != null? new Name(user): null;
+    }
+
+    public boolean isSubscribed() {
+        return currentCourse.getParticipants().contains(getCurrentUser());
+    }
+
+    public void setCurrentCourse(Course course) {
+        this.currentCourse = course;
+    }
+
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
