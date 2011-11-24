39db6b3b5f68f936d2dd2082bbe3b47c032a7129
Arne Limburg <arnelim@users.sourceforge.net>
2011-11-24 20:53:13 +0000

    Implemented CdiUserService

---
+++ jpasecurity-samples/jpasecurity-jsf-elearning-sample/jpasecurity-jsf-elearning-domain/src/main/java/net/sf/jpasecurity/sample/elearning/domain/CdiUserService.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2011 Arne Limburg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.sf.jpasecurity.sample.elearning.domain;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+
+/**
+ * @author Arne Limburg
+ */
+@Named("userService")
+public class CdiUserService implements UserService {
+
+    @Inject
+    private Provider<User> userProvider;
+    @Inject
+    private Provider<Course> courseProvider;
+
+    public boolean isSubscribed() {
+        return courseProvider.get().getParticipants().contains(userProvider.get());
+    }
+}
