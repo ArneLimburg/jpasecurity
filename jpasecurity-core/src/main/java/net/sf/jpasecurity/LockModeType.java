e06fc6842bef9afa08c302cd62a9dfb9c7ea1359
Arne Limburg <arnelim@users.sourceforge.net>
2013-02-25 12:32:28 +0000

    Updated LockModeType to JPA 2

--- jpasecurity-core/src/main/java/net/sf/jpasecurity/LockModeType.java
+++ jpasecurity-core/src/main/java/net/sf/jpasecurity/LockModeType.java
@@ -19,5 +19,12 @@ package net.sf.jpasecurity;
  * @author Arne Limburg
  */
 public enum LockModeType {
-    READ, WRITE;
+    READ,
+    WRITE,
+    OPTIMISTIC,
+    OPTIMISTIC_FORCE_INCREMENT,
+    PESSIMISTIC_READ,
+    PESSIMISTIC_WRITE,
+    PESSIMISTIC_FORCE_INCREMENT,
+    NONE;
 }
